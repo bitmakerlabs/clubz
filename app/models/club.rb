34a98d1bc47ebf62593a54b19340a42f03582dee
Devon Noel de Tilly <devon.noeldetilly@gmail.com>
2018-10-05 12:00:19 -0400

    In class solution

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,3 +1,11 @@
 class Club < ActiveRecord::Base
   belongs_to :user
+
+  def self.allowed_roles
+    ["wizard", "hobbit"]
+  end
+
+  def self.disallowed_roles
+    ["droid", "gangster"]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
   resources :clubs
-  resources :sessions, only: [:new, :create, :destroy]  
+  resources :sessions, only: [:new, :create, :destroy]
 
   root 'clubs#index'
-
+  
 end

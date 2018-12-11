b16939c74cff26a63948b7d3af1ce16d6fed67ec
Devon Noel de Tilly <devon.noeldetilly@gmail.com>
2018-12-11 17:54:10 -0500

    Revert "In class solution"
    This reverts commit 34a98d1bc47ebf62593a54b19340a42f03582dee.

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -1,22 +1,10 @@
 class ClubsController < ApplicationController
 
-  before_action :require_login, except: [:index]
-  before_action :require_role, only: [:show]
-  before_action :require_ownership, only: [:edit, :update]
-
   def index
     @clubs = Club.all
   end
 
   def show
-    # if session[:user_id] #logged in
-    #   @club = Club.find(params[:id])
-    #   render :show
-    # else #not logged in
-    #   flash[:alert] = ["You must login first"]
-    #   redirect_to new_session_path
-    # end
-
     @club = Club.find(params[:id])
   end
 
@@ -40,38 +28,17 @@ class ClubsController < ApplicationController
   end
 
   def edit
+    @club = Club.find(params[:id])
   end
 
   def update
+    @club = Club.find(params[:id])
+
     if @club && @club.update(name: params[:club][:name], description: params[:club][:description], user: current_user)
       redirect_to root_path
     else
       flash.now[:alert] = @club.errors.full_messages
       render :edit
-    end
-  end
-
-  private
-
-  def require_login
-    if !session[:user_id]
-      flash[:alert] = ["You must login first"]
-      redirect_to new_session_path
-    end
-  end
-
-  def require_ownership
-    @club = Club.find(params[:id])
-    if current_user != @club.user
-      flash[:alert] = ["You must be the owner of this club to edit it"]
-      redirect_to root_path
-    end
-  end
-
-  def require_role
-    if Club.disallowed_roles.include?(current_user.role)
-      flash[:alert] = ["#{current_user.role.capitalize}s are not allowed into clubs"]
-      redirect_to root_path
     end
   end
 
--- app/models/club.rb
+++ app/models/club.rb
@@ -1,11 +1,3 @@
 class Club < ActiveRecord::Base
   belongs_to :user
-
-  def self.allowed_roles
-    ["wizard", "hobbit"]
-  end
-
-  def self.disallowed_roles
-    ["droid", "gangster"]
-  end
 end
